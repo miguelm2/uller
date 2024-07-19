da2c3a26c756aa5bf05309e292229d0dd7157c49
DanielRivas05 <danielrivaskondory@gmail.com>
2024-07-19 17:20:37 -0500

    avancesuller
    Se realizaron avances en la pagina de uller

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,12 +9,61 @@
     <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
+
 <body>
-    <div>
-        <?php include_once ('./assets/html/header.php') ?>   
-    </div>
+    <main class="main">
+        <?php include_once('./assets/html/header.php') ?>
+        <section class="hero">
+            <div class="row">
+                <div class="col-md-6 text-left">
+                    <h1 class="mt-8 colorp1">
+                        ¡La solución para el mantenimiento de aires acondicionados en casa!
+                    </h1>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 text-center">
+                        <h1 class="mt-5 text-black">
+                            "Contrata ULLER la plataforma para suministro de técnicos certificados en Aire Acondicionado a tu hogar."
+                        </h1>
+                    </div>
+                    <div class="col-md-4 text-center">
+                        <img src="/assets/img/tecnico-uller.png" alt="tecnico uller" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 text-center">
+                        <h1 class="mt-6 colorp1">
+                            Beneficios de Uller
+                        </h1>
+                    </div>
+                    <div class="col-md-12">
+                        <ul class="mt-6 text-black">
+                            <li>Rutinas de acuerdo a recomendaciones y estándares internacionales*</li>
+                            <li>Tarifas estandarizadas para los servicios de mantenimiento de equipos menores a 5TR con condiciones de instalación convencionales (unidad interior y exterior a menos de 5 metros , fácil acceso a unidad exterior, unidad interior a menos de 2.2 de altura)</li>
+                            <li>Técnicos validados con estudio de seguridad y verificación de experiencia y conocimientos.</li>
+                            <li>Mantenimientos correctivos con técnicos de alta calidad y Soporte de ingeniería en caso de requerirlo.</li>
+                            <li>Rapidez en el servicio pues contamos con varios técnicos afiliados en las ciudades donde estamos.</li>
+                            <li>Seguridad y facilidad de pago por transferencia o con tarjeta de crédito con link de pago.</li>
+                            <li>Evite riesgos innecesarios por accidentes laborales sin tener cobertura nuestros Técnicos cuentan con seguridad social al día.</li>
+                            <li>Le recordaremos a tiempo para que tome sus mantenimientos preventivos y evite mayor consumo de energía o fallas comunes por descuido.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>    
+        </section>
+        <?php include_once('./assets/html/footer.php') ?>
+    </main>
     <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/vendor/jquery/jquery.min.js"></script>
-    
+
 </body>
+
 </html>
