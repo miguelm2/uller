5f47de88fa6a41064c535229e1e2356734f9c14e
DanielRivas05 <danielrivaskondory@gmail.com>
2024-07-24 17:01:47 -0500

    avancespaguller
    Se añadieron cambios en la pagina de uller

--- assets/html/header.php
+++ assets/html/header.php
@@ -4,14 +4,9 @@
             <a href="index" class="navbar-brand d-flex align-items-center mb-md-0 text-decoration-none">
                 <img src="assets/img/logo-web-3.png" alt="logo" class="img-fluid" style="max-width: 100px;">
             </a>
-
-            <a class="navbar-toggler position-absolute right-0 border-0 " href="#offcanvasWithBackdrop" role="button" aria-label="Menu desplegable para movil">
-
-                <span data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop"
-                    aria-controls="offcanvasWithBackdrop">
-                    <strong><i style="font-size: 28px;color: #111827;" class="bi bi-list-ul fw-blod"></i></strong>
-                </span>
-            </a>
+            <button class="btn btn-primary d-xl-none ms-auto" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
+                <i class="bi bi-list"></i>
+            </button>
 
             <div class="collapse navbar-collapse justify-content-end">
                 <ul class="nav col-12 col-md-auto justify-content-center main-menu">
@@ -20,63 +15,28 @@
                     <li><a href="bioseguridad" class="nav-link">Bioseguridad</a></li>
                     <li><a href="faq" class="nav-link">FAQ</a></li>
                     <li><a href="contactenos" class="nav-link">Contáctenos</a></li>
-                    <li><a href="contact-us" class="nav-link">Blog</a></li>
+                    <li><a href="https://www.uller.co/blog/" class="nav-link">Blog</a></li>
                     <li><a href="https://www.uller.co/system/views/page/login" class="nav-link">Ingresar</a></li>
                     <li><a href="/system/views/page/login" class="nav-link">Registrarse</a></li>
-
                 </ul>
             </div>
-
         </div>
     </nav>
-    <!--offcanvas menu start-->
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasWithBackdrop">
-        <div class="offcanvas-header d-flex align-items-center mt-4">
-            <a href="index" class="d-flex align-items-center mb-md-0 text-decoration-none">
-                <img src="assets/img/logo-kondory.webp" alt="logo" class="img-fluid ps-2">
-            </a>
-            <button type="button" name="closed" id="closed" class="close-btn text-danger" data-bs-dismiss="offcanvas" aria-label="Close"><i
-                    class="bi bi-x-circle"></i></button>
-        </div>
+    <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop">
         <div class="offcanvas-body">
-            <ul class="nav col-12 col-md-auto justify-content-center main-menu text-primary">
-                <li><a href="about-us" class="nav-link">Nosotros</a></li>
-                <li><a href="services" class="nav-link">Servicios</a></li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">Soluciones</a>
-                    <div class="dropdown-menu border-0 rounded-custom shadow py-0 bg-white" style="display: none;">
-                        <div class="dropdown-grid rounded-custom width-full-2">
-                            <div class="dropdown-grid-item">
-                                <h6 class="drop-heading">Software</h6>
-                                <a href="chatbot" class="dropdown-link">
-                                    <span class="me-2"><i class="bi bi-robot"></i></span>
-                                    <div class="drop-title">ChatBot con Inteligencia Artificial</div>
-                                </a>
-                                <a href="biomantenimiento" class="dropdown-link">
-                                    <span class="me-2"><i class="bi bi-motherboard"></i></span>
-                                    <div class="drop-title">Biomantenimiento - Software Mantenimientos</div>
-                                </a>
-                                <a href="inventario" class="dropdown-link">
-                                    <span class="me-2"><i class="bi bi-collection"></i></span>
-                                    <div class="drop-title">Software de Inventario y Ventas</div>
-                                </a>
-                                <a href="webs" class="dropdown-link">
-                                    <span class="me-2"><i class="bi bi-globe"></i></span>
-                                    <div class="drop-title">Paginas web y Tiendas Online</div>
-                                </a>
-                            </div>
-
-
-                        </div>
-                    </div>
-                </li>
-                <li><a href="portfolio" class="nav-link">Casos de exito</a></li>
-                <li><a href="blog" class="nav-link">Blog</a></li>
-                <li><a href="contact-us" class="nav-link">Contacto</a></li>
+            <ul class="nav flex-column">
+                <li class="nav-item"><a href="quienes-somos">Quienes somos</a></li>
+                <li class="nav-item"><a href="nuestro-proposito">Nuestro Propósito</a></li>
+                <li class="nav-item"><a href="bioseguridad">Bioseguridad</a></li>
+                <li class="nav-item"><a href="faq">FAQ</a></li>
+                <li class="nav-item"><a href="contactenos">Contáctenos</a></li>
+                <li class="nav-item"><a href="https://www.uller.co/blog/">Blog</a></li>
+                <li class="nav-item"><a href="https://www.uller.co/system/views/page/login">Ingresar</a></li>
+                <li class="nav-item"><a href="/system/views/page/login">Registrarse</a></li>
             </ul>
         </div>
-
     </div>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.1.0/js/bootstrap.bundle.min.js"></script>
     <!--offcanvas menu end-->
 </header>
